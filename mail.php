46136e344e3cb908b7babebd1af4b8a1dd9d6c61
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2023-08-16 17:03:30 -0400

    fix

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $to = "vijaynannapuraju@gmail.com";
 
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone Number =" . $number;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@vijayn7.github.io" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
